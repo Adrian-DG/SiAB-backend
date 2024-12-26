67c6dc78b610adff31f401a5b07af44180d1ef41
Adrian-DG <adrian96guilamo@gmail.com>
2024-12-26 11:41:41 -0400

    ..

--- SiAB.API/Program.cs
+++ SiAB.API/Program.cs
@@ -74,10 +74,7 @@ if (builder.Environment.IsDevelopment())
 {
 	app.UseDeveloperExceptionPage();
 	app.UseSwagger();
-	app.UseSwaggerUI(c => {
-		c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
-		c.RoutePrefix = string.Empty;
-	});
+	app.UseSwaggerUI();
 }
 else
 {
