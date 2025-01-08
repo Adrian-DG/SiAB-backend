c8717ea934b74dfe8f6b532d90cba12fe8239c17
Adrian-DG <adrian96guilamo@gmail.com>
2025-01-07 23:14:32 -0400

    Add new users and roles to UserSeeder for initial setup
    The code changes in the `UserSeeder.cs` file within the `SiAB.Infrastructure.Data.Seed` namespace involve adding five new `Usuario` objects with unique details such as `Cedula`, `Nombre`, `Apellido`, `UserName`, `NormalizedUserName`, `Institucion`, and `PasswordHash`. Corresponding `IdentityUserRole<int>` entries are also added to assign the role with `RoleId` 1 to each of these new users. This enhances the initial data seeding process by providing more predefined users and their roles, which can be useful for testing or initial setup purposes.

--- SiAB.Infrastructure/Data/Seed/UserSeeder.cs
+++ SiAB.Infrastructure/Data/Seed/UserSeeder.cs
@@ -28,10 +28,59 @@ namespace SiAB.Infrastructure.Data.Seed
 					NormalizedUserName = "ADMIN",
 					Institucion = InstitucionEnum.MIDE,
 					PasswordHash = passwordHasher.HashPassword(null, "admin01")
-				});
+				},
+				new Usuario
+				{
+					Id = 2,
+					Cedula = "0000000001",
+					Nombre = "MIDE",
+					Apellido = "MIDE",
+					UserName = "mide",
+					NormalizedUserName = "MIDE",
+					Institucion = InstitucionEnum.MIDE,
+					PasswordHash = passwordHasher.HashPassword(null, "mide01")
+				},
+				new Usuario
+				{
+					Id = 3,
+					Cedula = "0000000002",
+					Nombre = "ERD",
+					Apellido = "ERD",
+					UserName = "erd",
+					NormalizedUserName = "ERD",
+					Institucion = InstitucionEnum.ERD,
+					PasswordHash = passwordHasher.HashPassword(null, "erd01")
+				},
+				new Usuario
+				{
+					Id = 4,
+					Cedula = "0000000003",
+					Nombre = "ARD",
+					Apellido = "ARD",
+					UserName = "ard",
+					NormalizedUserName = "ARD",
+					Institucion = InstitucionEnum.ARD,
+					PasswordHash = passwordHasher.HashPassword(null, "ard01")
+				},
+				new Usuario
+				{
+					Id = 5,
+					Cedula = "0000000004",
+					Nombre = "FARD",
+					Apellido = "FARD",
+					UserName = "fard",
+					NormalizedUserName = "FARD",
+					Institucion = InstitucionEnum.FARD,
+					PasswordHash = passwordHasher.HashPassword(null, "fard01")
+				}
+				);
 
 			builder.Entity<IdentityUserRole<int>>().HasData(
-				new IdentityUserRole<int> { UserId = 1, RoleId = 1 }
+				new IdentityUserRole<int> { UserId = 1, RoleId = 1 },
+				new IdentityUserRole<int> { UserId = 2, RoleId = 1 },
+				new IdentityUserRole<int> { UserId = 3, RoleId = 1 },
+				new IdentityUserRole<int> { UserId = 4, RoleId = 1 },
+				new IdentityUserRole<int> { UserId = 5, RoleId = 1 }
 			);
 		}
 	}
