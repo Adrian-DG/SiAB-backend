97fe9280aaa9f731c1a851fc35a97a4e7c60741b
Adrian-DG <adrian96guilamo@gmail.com>
2025-01-22 01:03:26 -0400

    Add new endpoints for generating weapon relationship templates
    - Added three new endpoints in ExcelTemplateController:
      - GetPlantillaRelacionArmasMilitares (renamed from GetPlantillaRelacionArmas)
      - GetPlantillaRelacionArmasCiviles
      - GetPlantillaRelacionArmasDependencias
    - Updated routes and filenames for generated Excel files.
    - Added new column "propiedad" to military template.
    - Applied additional header styling to civilian and dependencies templates.

--- SiAB.API/Controllers/Reports/ExcelTemplateController.cs
+++ SiAB.API/Controllers/Reports/ExcelTemplateController.cs
@@ -16,8 +16,8 @@ namespace SiAB.API.Controllers.Reports
 		{
 		}
 
-		[HttpGet("plantilla-relacion-armas")]
-		public IActionResult GetPlantillaRelacionArmas()
+		[HttpGet("plantilla-relacion-armas-militares")]
+		public IActionResult GetPlantillaRelacionArmasMilitares()
 		{
 			using (ExcelPackage excel = new ExcelPackage())
 			{				
@@ -25,7 +25,7 @@ namespace SiAB.API.Controllers.Reports
 
 				// Header
 
-				var columns = new string[] { "cedula", "tipo", "marca", "modelo", "calibre", "serie", "formulario53", "fechaEfectividad" };
+				var columns = new string[] { "cedula", "tipo", "marca", "modelo", "calibre", "serie", "propiedad", "formulario53", "fechaEfectividad" };
 				var header = workSheet.Cells[1, 1, 1, columns.Length];
 
 				for (var i = 0; i < columns.Length; i++)
@@ -41,7 +41,67 @@ namespace SiAB.API.Controllers.Reports
 				{ 
 					excel.SaveAs(ms);
 					byte[] file = ms.ToArray();
-					return File(file, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "plantilla_relacion_armas.xlsx");
+					return File(file, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "plantilla_relacion_armas_militares.xlsx");
+				}
+			}
+		}
+
+		[HttpGet("plantilla-relacion-armas-civiles")]
+		public IActionResult GetPlantillaRelacionArmasCiviles()
+		{
+			using (ExcelPackage excel = new ExcelPackage())
+			{
+				var workSheet = excel.Workbook.Worksheets.Add("relacion #1");
+
+				// Header
+
+				var columns = new string[] { "cedula", "tipo", "marca", "modelo", "calibre", "serie", "propiedad", "formulario53", "fechaEfectividad" };
+				var header = workSheet.Cells[1, 1, 1, columns.Length];
+
+				for (var i = 0; i < columns.Length; i++)
+				{
+					workSheet.Cells[1, i + 1].Value = columns[i];
+				}
+
+				header.Style.Font.Bold = true;
+				header.Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
+				header.Style.Fill.BackgroundColor.SetColor(System.Drawing.Color.LightGray);
+
+				using (MemoryStream ms = new MemoryStream())
+				{
+					excel.SaveAs(ms);
+					byte[] file = ms.ToArray();
+					return File(file, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "plantilla_relacion_armas_civiles.xlsx");
+				}
+			}
+		}
+
+		[HttpGet("plantilla-relacion-armas-dependencias")]
+		public IActionResult GetPlantillaRelacionArmasDependencias()
+		{
+			using (ExcelPackage excel = new ExcelPackage())
+			{
+				var workSheet = excel.Workbook.Worksheets.Add("relacion #1");
+
+				// Header
+
+				var columns = new string[] { "cedula", "tipo", "marca", "modelo", "calibre", "serie", "propiedad", "formulario53", "fechaEfectividad" };
+				var header = workSheet.Cells[1, 1, 1, columns.Length];
+
+				for (var i = 0; i < columns.Length; i++)
+				{
+					workSheet.Cells[1, i + 1].Value = columns[i];
+				}
+
+				header.Style.Font.Bold = true;
+				header.Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
+				header.Style.Fill.BackgroundColor.SetColor(System.Drawing.Color.LightGray);
+
+				using (MemoryStream ms = new MemoryStream())
+				{
+					excel.SaveAs(ms);
+					byte[] file = ms.ToArray();
+					return File(file, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "plantilla_relacion_armas_dependencias.xlsx");
 				}
 			}
 		}
