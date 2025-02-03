694f573bdacd75662fc2402f6fc6a0ecbca97325
Adrian-DG <adrian96guilamo@gmail.com>
2025-02-02 22:31:51 -0400

    Refactor enums and add necessary using directives
    Added using directives for System namespaces in TipoLicenciaEnum.cs, TipoOrigenDestinoEnum.cs, and TipoTransaccionEnum.cs. Introduced new enums TipoLicenciaEnum and TipoOrigenDestinoEnum in SiAB.Core.Enums. Updated TipoTransaccionEnum with new values and removed old ones. Removed UsuarioRole enum from SiAB.Core.Enums.

---
+++ SiAB.Core/Enums/TipoLicenciaEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiAB.Core.Enums
+{
+	public enum TipoLicenciaEnum
+	{
+		IMPORTAR = 1,
+		EXPORTAR = 2,
+		IMPORTAR_EXPORTAR = 3
+	}
+}
