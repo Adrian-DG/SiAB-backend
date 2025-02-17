4b044f93547417a2d467d8b11c94a59a78e4bf9c
Adrian Jose Diaz Guilamo <ajdiaz@ADRIANDIAZ>
2025-02-17 11:13:13 -0400

    Añadir método GetArticulos en ArticulosController
    Se han incorporado nuevas importaciones para las entidades y DTOs necesarios. Se implementó el método `GetArticulos` que permite obtener una lista paginada de artículos con filtros de búsqueda, seleccionando propiedades específicas y devolviendo los resultados en formato JSON.

--- SiAB.API/Controllers/Inventario/ArticulosController.cs
+++ SiAB.API/Controllers/Inventario/ArticulosController.cs
@@ -2,7 +2,11 @@
 using Microsoft.AspNetCore.Mvc;
 using SiAB.API.Helpers;
 using SiAB.Application.Contracts;
+using SiAB.Core.DTO;
 using SiAB.Core.Entities.Inventario;
+using SiAB.Core.Entities.Misc;
+using SiAB.Core.Enums;
+using System.Linq.Expressions;
 
 namespace SiAB.API.Controllers.Inventario
 {
@@ -12,6 +16,34 @@ namespace SiAB.API.Controllers.Inventario
 	{
 		public ArticulosController(IUnitOfWork unitOfWork, IMapper mapper, IUserContextService userContextService) : base(unitOfWork, mapper, userContextService)
 		{
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> GetArticulos([FromQuery] PaginationFilter filter)
+		{
+			var result = await _uow.Repository<Articulo>().GetListPaginateAsync(
+					includes: new Expression<Func<Articulo, object>>[] { 
+						m => m.Marca, 
+						m => m.Modelo,
+						m => m.Categoria,
+						m => m.Calibre,
+						m => m.SubTipo,
+						m => m.Tipo
+					},
+					predicate: a => a.Serie.Contains(filter.SearchTerm ?? "") && a.CodInstitucion == (InstitucionEnum)_codInstitucionUsuario,
+					selector: a => new
+					{
+						Id = a.Id,
+						Categoria = a.Categoria.Nombre,
+						Tipo = a.Tipo.Nombre,
+						SubTipo = a.SubTipo.Nombre,
+						Marca = a.Marca.Nombre,
+						Modelo = a.Modelo.Nombre,
+						Serie = a.Serie,
+					}
+				);
+
+			return new JsonResult(result);
 		}
 	}
 }
