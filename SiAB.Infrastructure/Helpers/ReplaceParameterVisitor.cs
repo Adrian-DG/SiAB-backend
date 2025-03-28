2dfcb7610a0f5b4cecd9c4c13bbe4f35858157c2
Adrian-DG <adrian96guilamo@gmail.com>
2025-03-28 03:20:18 -0400

    Add PredicateHelper and ReplaceParameterVisitor classes
    Introduce `PredicateHelper` for combining predicate expressions and `ReplaceParameterVisitor` for parameter replacement in expression trees. These additions enhance the functionality of the `SiAB.Infrastructure.Helpers` namespace.

---
+++ SiAB.Infrastructure/Helpers/ReplaceParameterVisitor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiAB.Infrastructure.Helpers
+{
+	public class ReplaceParameterVisitor : ExpressionVisitor
+	{
+		private readonly ParameterExpression _oldParameter;
+		private readonly ParameterExpression _newParameter;
+
+		public ReplaceParameterVisitor(ParameterExpression oldParameter, ParameterExpression newParameter)
+		{
+			_oldParameter = oldParameter;
+			_newParameter = newParameter;
+		}
+
+		protected override Expression VisitParameter(ParameterExpression node)
+		{
+			return node == _oldParameter ? _newParameter : base.VisitParameter(node);
+		}
+	}
+}
