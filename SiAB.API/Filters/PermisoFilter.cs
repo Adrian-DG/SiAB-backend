976d66eebab457ae17c90b3e410ce9feac53fd6d
Adrian-DG <adrian96guilamo@gmail.com>
2025-03-28 03:20:01 -0400

    Add PermisoFilter and user roles management
    - Introduced `PermisoFilter` class for role-based access control using `IAsyncActionFilter`.
    - Added `Roles` property to `IUserContextService` for storing user roles.
    - Implemented `Roles` property in `UserContextService`, initialized to an empty array.
    - Defined a new constant `Roles` in `TokenPropertiesContants` for token processing.

---
+++ SiAB.API/Filters/PermisoFilter.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using SiAB.API.Helpers;
+using SiAB.Core.Constants;
+
+namespace SiAB.API.Filters
+{
+	public class PermisoFilter : IAsyncActionFilter
+	{
+		private readonly IUserContextService _userContextService;
+
+		public PermisoFilter(IUserContextService userContextService)
+		{
+			_userContextService = userContextService;
+		}
+
+		public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+		{
+			string? rolesString = context.HttpContext.User.FindFirst(TokenPropertiesContants.Roles)?.Value;
+
+			if (string.IsNullOrEmpty(rolesString)) throw new System.Exception("No se encontró el código de usuario en el token.");
+
+			string[] roles = rolesString.Split(',');
+
+			if (!roles.Any()) throw new System.Exception("No se encontró el código de usuario en el token.");
+
+			_userContextService.Roles = roles;
+
+			await next();
+		}
+	}
+}
