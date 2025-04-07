7011878ada63040d920c7bf64bdd882e696178e8
Adrian Jose Diaz Guilamo <ajdiaz@ADRIANDIAZ>
2025-04-07 07:49:05 -0400

    Actualiza roles del usuario con UserId = 6
    Se ha eliminado la asignación del rol `MODULO_EMPRESAS` para el usuario con `UserId = 6`. Se han añadido múltiples roles nuevos: `MODULO_EMPRESAS`, `MODULO_MANTENIMIENTO`, `MANTENIMIENTO_TIPOS`, `MANTENIMIENTO_SUBTIPOS` y `MANTENIMIENTO_TIPOS_DOCUMENTOS`.

--- SiAB.Infrastructure/Data/Seed/UserSeeder.cs
+++ SiAB.Infrastructure/Data/Seed/UserSeeder.cs
@@ -95,7 +95,12 @@ namespace SiAB.Infrastructure.Data.Seed
 				new IdentityUserRole<int> { UserId = 3, RoleId = (int)UsuarioRolesEnum.ADMINISTRADOR_GENERAL },
 				new IdentityUserRole<int> { UserId = 4, RoleId = (int)UsuarioRolesEnum.ADMINISTRADOR_GENERAL },
 				new IdentityUserRole<int> { UserId = 5, RoleId = (int)UsuarioRolesEnum.ADMINISTRADOR_GENERAL },
-				new IdentityUserRole<int> { UserId = 6, RoleId = (int)UsuarioRolesEnum.MODULO_EMPRESAS }
+				
+				new IdentityUserRole<int> { UserId = 6, RoleId = (int)UsuarioRolesEnum.MODULO_EMPRESAS },
+				new IdentityUserRole<int> { UserId = 6, RoleId = (int)UsuarioRolesEnum.MODULO_MANTENIMIENTO },
+				new IdentityUserRole<int> { UserId = 6, RoleId = (int)UsuarioRolesEnum.MANTENIMIENTO_TIPOS },
+				new IdentityUserRole<int> { UserId = 6, RoleId = (int)UsuarioRolesEnum.MANTENIMIENTO_SUBTIPOS },
+				new IdentityUserRole<int> { UserId = 6, RoleId = (int)UsuarioRolesEnum.MANTENIMIENTO_TIPOS_DOCUMENTOS }
 			);
 		}
 	}
