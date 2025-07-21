6d28712a6804c83115f499c9868d2b373af39f94
ajdiaz <ajdiaz@mide.local>
2025-07-21 09:46:03 -0400

    Add auditing properties to entity metadata classes
    This commit introduces auditing properties to the `AuditableEntityMetadata` class and the `IAuditableEntityMetadata` interface. The new properties include `UsuarioCreadorId`, `UsuarioCreadorCodInstitucion`, `FechaCreacion`, `UsuarioEditorId`, `UsuarioEditorCodInstitucion`, and `FechaModificacion`, enabling tracking of entity creators and editors along with creation and modification timestamps. Necessary namespaces have also been imported to support these changes.

---
+++ SiAB.Core/Abstraction/Auditable/AuditableEntityMetadata.cs
@@ -0,0 +1,32 @@
+﻿using SiAB.Core.Entities.Auth;
+using SiAB.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiAB.Core.Abstraction.Auditable
+{
+	public abstract class AuditableEntityMetadata : EntityMetadata, IAuditableEntityMetadata
+	{
+		// auditables 
+
+		// Creador
+
+		[ForeignKey(nameof(UsuarioCreadorId))]
+		public int UsuarioCreadorId { get; set; }
+		public virtual Usuario? UsuarioCreador { get; set; }
+		public InstitucionEnum UsuarioCreadorCodInstitucion { get; set; }
+		public DateTime FechaCreacion { get; set; }
+
+		// Editor
+
+		[ForeignKey(nameof(UsuarioEditorId))]
+		public int? UsuarioEditorId { get; set; }
+		public virtual Usuario? UsuarioEditor { get; set; }
+		public InstitucionEnum UsuarioEditorCodInstitucion { get; set; }
+		public DateTime? FechaModificacion { get; set; }
+	}
+}
---
+++ SiAB.Core/Abstraction/Auditable/IAuditableEntityMetadata.cs
@@ -0,0 +1,22 @@
+﻿using SiAB.Core.Entities.Auth;
+using SiAB.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiAB.Core.Abstraction.Auditable
+{
+	public interface IAuditableEntityMetadata
+	{
+		public int UsuarioCreadorId { get; set; }
+		public InstitucionEnum UsuarioCreadorCodInstitucion { get; set; }
+		public DateTime FechaCreacion { get; set; }
+
+		public int? UsuarioEditorId { get; set; }
+		public InstitucionEnum UsuarioEditorCodInstitucion { get; set; }
+		public DateTime? FechaModificacion { get; set; }
+	}
+}
